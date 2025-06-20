27807b502888e70649a7683857260327b0d001fc
ZarcoVM <angelmanuelzarco@gmail.com>
2025-06-20 07:31:58 -0600

    first commit

---
+++ commits.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for month in 05 06
+do
+  if [ "$month" == "05" ]; then
+    max_day=31
+  else
+    max_day=20
+  fi
+
+  for day in $(seq -w 1 $max_day)
+  do
+    for commit in {1..10}
+    do
+      export GIT_COMMITTER_DATE="2024-$month-$day 12:$commit:00"
+      export GIT_AUTHOR_DATE="2024-$month-$day 12:$commit:00"
+      echo "Commit $commit del día $day/$month" >> archivo.txt
+      git add archivo.txt
+      git commit -m "Commit $commit del día $day/$month"
+    done
+  done
+done
