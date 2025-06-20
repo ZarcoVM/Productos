3b014301d9076b29c650dcc848333dea66395846
ZarcoVM <angelmanuelzarco@gmail.com>
2025-06-20 07:40:09 -0600

    first commit

--- commits.sh
+++ commits.sh
@@ -12,11 +12,11 @@ do
   do
     for commit in {1..10}
     do
-      export GIT_COMMITTER_DATE="2024-$month-$day 12:$commit:00"
-      export GIT_AUTHOR_DATE="2024-$month-$day 12:$commit:00"
-      echo "Commit $commit del día $day/$month" >> archivo.txt
+      export GIT_COMMITTER_DATE="2025-$month-$day 12:$commit:00"
+      export GIT_AUTHOR_DATE="2025-$month-$day 12:$commit:00"
+      echo "Commit $commit del día $day/$month/2025" >> archivo.txt
       git add archivo.txt
-      git commit -m "Commit $commit del día $day/$month"
+      git commit -m "Commit $commit del día $day/$month/2025"
     done
   done
 done
